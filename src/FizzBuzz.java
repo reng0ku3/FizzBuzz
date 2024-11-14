57507bb8455d53a7d2941e61a50b8beb3a3be117
SuzukiKohei <188088383+reng0ku3@users.noreply.github.com>
2024-11-14 22:59:50 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,20 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		String str1 = "Fizz";
+		String str2 = "Buzz";
+		String str = str1 + str2;
+		for(int i = 1; i <= 100; i++) {
+			if(i % (3 * 5) == 0) {
+				System.out.println(str);
+			}else if(i % 3 == 0) {
+				System.out.println(str1);
+			}else if(i % 5 == 0) {
+				System.out.println(str2);
+			}else {
+				System.out.println(i);
+			}
+		}
 	}
 
 }
